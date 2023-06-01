8aa97e45a5aa19ab1c8fab5e8cd459ea8269ebca
suhail <mohamed.s@smartladders.com>
2023-06-01 09:23:53 +0530

    fiorsst

--- index.js
+++ index.js
@@ -13,17 +13,44 @@ app.listen(PORT, () => {
 
 
 app.get('/index', function (req, res) {  
-    (async function example() {
-        let driver = await new Builder().forBrowser(Browser.CHROME).build();
-        try {
-          await driver.get('https://www.google.com/ncr');
-          await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-          await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-        } finally {
-          await driver.quit();
-        }
-      })();
-      example();
+    const { Builder, By, Key, until } = require('selenium-webdriver');
+    const chrome = require('selenium-webdriver/chrome');
+    
+    // Set up Chrome options
+    const chromeOptions = new chrome.Options();
+    chromeOptions.addArguments('--headless'); // Run in headless mode
+    
+    // Create a new WebDriver instance
+    const driver = new Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(chromeOptions)
+      .build();
+    
+    // Perform actions
+    async function performActions() {
+      try {
+        // Navigate to a webpage
+        await driver.get('https://www.google.com');
+    
+        // Find an element and interact with it
+        const searchInput = await driver.findElement(By.name('q'));
+        await searchInput.sendKeys('Selenium', Key.RETURN);
+    
+        // Wait for search results to load
+        await driver.wait(until.titleContains('Selenium'), 5000);
+    
+        // Print the page title
+        const pageTitle = await driver.getTitle();
+        console.log('Page title:', pageTitle);
+      } finally {
+        // Quit the WebDriver session
+        await driver.quit();
+      }
+    }
+    
+    // Call the function to execute the actions
+    performActions();
+    
 })
 app.get("/", async (request, response) => {
       response.status(500).send("suhail");
